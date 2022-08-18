f20206d538bfaacc92f94aaeb4802c4d26b88c31
SandrLite <mr.wlas@mail.ru>
2022-08-18 10:42:07 +0300

    7 Задание

--- seminar.py
+++ seminar.py
@@ -53,15 +53,29 @@
 #     is_multiple(x,l)
 
 
-print('6. Дано число обозначающее день недели. Вывести его название и указать является ли он выходным:')
+# print('6. Дано число обозначающее день недели. Вывести его название и указать является ли он выходным:')
 
-def getWeekDate(day):
-    days=["Рождение дня недели =)", "Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]    
-    return days[day]
+# def getWeekDate(day):
+#     days=["Рождение дня недели =)", "Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]    
+#     return days[day]
 
-day = int(input('Укажите день недели цыфрой: '))
-week = getWeekDate(day)
-if(week == "Суббота" or week == "Воскресенье"):
-    print(f'День недели {week} -  выходной')
+# day = int(input('Укажите день недели цыфрой: '))
+# week = getWeekDate(day)
+# if(week == "Суббота" or week == "Воскресенье"):
+#     print(f'День недели {week} -  выходной')
+# else:
+#     print(f'День недели {week}')
+
+
+print('7. Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат:')
+
+def logical_statement(x, y, z):
+    print(f"¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} is {(not (x or y or z)) == (not x and not y and not z)}")
+    return (not (x or y or z)) == (not x and not y and not z)
+
+if (logical_statement(0, 0, 0) and logical_statement(0, 0, 1) and logical_statement(0, 1, 0) and 
+logical_statement(0, 1, 1) and logical_statement(1, 0, 0) and logical_statement(1, 0, 1) and
+logical_statement(1, 1, 0) and logical_statement(1, 1, 1)):
+    print("Истинно")
 else:
-    print(f'День недели {week}')
+    print("Ложно")
