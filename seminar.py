514a1a62e31c7962d2bcbcac3ad1fe1dd7e0c45d
SandrLite <mr.wlas@mail.ru>
2022-09-09 14:18:01 +0300

    17 Задание

--- seminar.py
+++ seminar.py
@@ -194,8 +194,36 @@
 
 # 16. Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму
 
-n = int(input("Введите число n: "))
-s = 0
-for i in range(1,n+1):
-    s += (1+1/i)**i
-print(f"Результат: {round(s,2)}")
+# n = int(input("Введите число n: "))
+# s = 0
+# for i in range(1,n+1):
+#     s += (1+1/i)**i
+# print(f"Результат: {round(s,2)}")
+
+# 17. Задать список из N элементов, заполненных числами из [-N, N]. 
+# Найти произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+import random
+def write_file(number):
+    with open('file.txt', 'w') as data:
+        for i in range(number):
+            data.write(f"{random.randrange(0, 2*number)}\n")
+
+
+def read_file():
+    with open('file.txt', 'r') as data:
+        indexs = list(map(int,data.readlines()))
+    return indexs
+
+N = int(input("Введите число N: "))
+lst_number = [i for i in range(-N, N+1)]
+
+write_file(N)
+lst_index = read_file()
+
+prod = 1
+for i in range(len(lst_index)):
+    prod *= lst_number[lst_index[i]]
+
+print(f"Результат: {prod}")
+
